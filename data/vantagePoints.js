ed52cabbb86eabb8e24f307d0b1462adf16f58b1
davetayls <dave@the-taylors.org>
2017-02-25 20:57:57 +0000

    new stuff

--- data/vantagePoints.js
+++ data/vantagePoints.js
@@ -6,6 +6,20 @@ export const vantagePointData = [
     description: `
 Walking down Conigre hill with the stone walls on each side you
 are suddenly welcomed by this magnificent view as the walls begin to lower.
+
+Conigre hill is situated on the west side of town, looking across the town
+to the hills beyond you can see the edge of Salisbury plain and the Westbury
+White Horse.
+
+From the left you can see the triangular roof of the library next to the
+river. The library is home to the Bradford on Avon Museum which holds a
+broad collection from our Roman, iron and rubber past to Moulton bicycles,
+brewing and pubs.
+
+In the middle of your view you can see the rooftop of Abbey Mill which has
+also seen time as a Cloth Mill and Rubber factory.
+
+To the right are views across Barton Farm and Frome Road.
 `,
     image: require('./images/1.jpg'),
     latitude: 51.349160,
@@ -17,17 +31,44 @@ are suddenly welcomed by this magnificent view as the walls begin to lower.
     description: `
 The often undiscovered gem, the view out over the Tithe Barn
 from the gardens of St Mary Tory.
+
+The name Tory comes from the old word 'tor' which means 'hill', and from this lofty vantage point you have a fabulous panorama across
+the whole town and beyond. From direct line of sight of the Tithe Barn
+to the River and playing fields.
+
+This whole area of Tory and Middle rank were gradually built over hundreds
+of years from the 1690s to the 19th century.
 `,
     image: require('./images/2.jpg'),
     latitude: 51.347,
     longitude: -2.2561,
     altitude: 78.86,
-    bearing: 201.59
+    bearing: 201.59,
+    related: [
+      {
+        "title": "Explore Bradford: Tory, Middle Rank & Budbury",
+        "type": "web",
+        "url": "http://www.bradfordonavonmuseum.co.uk/archives/3025"
+      },
+      {
+        "title": "Freshford website: Chapel of St. Mary Tory",
+        "type": "web",
+        "url": "http://www.freshford.com/13_st_mary_tory.htm"
+      }
+    ]
   },
   {
     id: 3,
     title: 'Across the river and canal',
-    description: "Take a moment out to watch the activity along the Barton farm walk, the Avon river and the canal",
+    description: `
+Take a moment out to watch the activity along the Barton farm walk,
+the Avon river and the canal. This is the less busy side of the river
+with a real sense of space as you drink in setting.
+
+Cross the packhorse bridge from near the farmhouse towards the rowing club
+building and the train track. As you reach the other side you will see a
+path going off to the left.
+`,
     image: require('./images/3.jpg'),
     latitude: 51.3439,
     longitude: -2.2586,
@@ -37,21 +78,127 @@ from the gardens of St Mary Tory.
   {
     id: 4,
     title: 'The Tithe Barn',
-    description: "With it's beautiful surroundings there are so many aspects to view the magnificent Tithe Barn",
+    description: `
+In amoungst it's beautiful surroundings within the 36-acre Barton Farm
+country park is the magnificent Tithe Barn. Dating from about 1340,
+it's magnificent timber roof is a sight to behold.
+`,
     image: require('./images/4.jpg'),
     latitude: 51.3431,
     longitude: -2.2557,
     altitude: 34.4,
-    bearing: 112.42
+    bearing: 112.42,
+    related: [
+      {
+        "title": "Bradford on Avon Museum: Tithe Barn",
+        "type": "web",
+        "url": "http://www.bradfordonavonmuseum.co.uk/tithe-barn"
+      },
+      {
+        "title": "English Heritage: Bradford on Avon Tithe Barn",
+        "type": "web",
+        "url": "http://www.english-heritage.org.uk/visit/places/bradford-on-avon-tithe-barn/history/"
+      }
+    ]
   },
   {
     id: 5,
     title: 'Budbury',
-    description: "You can access a footpath coming down in to town via Budbury Place. Following the path to the right you are teased with glimpses into the valley",
+    description: `
+The whole Budbury area is on an old Iron Age Fort site. The Budbury Monor
+House was mentioned in the Domesday Book in 1086.
+
+You can access a footpath coming down in to town at the end of Budbury Place.
+Alternatively you can come up from town via Conigre Hill and around the back
+of the houses on Tory.
+
+Following the path to the right you are teased with glimpses into the valley
+as you follow the winding paths down the hill.
+`,
     image: require('./images/5.jpg'),
     latitude: 51.3478,
     longitude: -2.2557,
     altitude: 34.4,
-    bearing: 112.42
+    bearing: 112.42,
+    related: [
+      {
+        "title": "Bradford on Avon Museum: Tory, Middle Rand & Budbury",
+        "type": "web",
+        "url": "http://www.bradfordonavonmuseum.co.uk/archives/3025"
+      }
+    ]
+  },
+  {
+    id: 6,
+    title: 'Up St Margaret\'s Steps',
+    description: `
+One of the most stunning panoramas from the southern end of the town can be
+seen from outside the Scout Hut at the top of St Margaret's Steps.
+
+St Margaret's Steps can be found hidden between the houses on Bridge Street
+just opposite the Library. Follow them to the top and turn left. You will
+soon come to the Scout Hut. A footpath to the side of the hut is a nice way
+to come back down through the trees to the Secret Wier.
+`,
+    image: require('./images/6.jpg'),
+    latitude: 51.3456,
+    longitude: -2.2497,
+    altitude: 46.57,
+    bearing: 344.48
+  },
+  {
+    id: 7,
+    title: 'At the Lock',
+    description: `
+`,
+    image: require('./images/7.jpg'),
+    latitude: 51.3414,
+    longitude: -2.2521,
+    altitude: 42.03,
+    bearing: 145.51
+  },
+  {
+    id: 8,
+    title: 'The Secret Wier',
+    description: `
+`,
+    image: require('./images/8.jpg'),
+    latitude: 51.3451,
+    longitude: -2.2472,
+    altitude: 39.36,
+    bearing: 95.57
+  },
+  {
+    id: 9,
+    title: 'Along the Canal',
+    description: `
+`,
+    image: require('./images/9.jpg'),
+    latitude: 51.3426,
+    longitude: -2.2557,
+    altitude: 37.73,
+    bearing: 266.73
+  },
+  {
+    id: 10,
+    title: 'At the Station',
+    description: `
+`,
+    image: require('./images/10.jpg'),
+    latitude: 51.3448,
+    longitude: -2.2518,
+    altitude: 34.11,
+    bearing: 277.21
+  },
+  {
+    id: 11,
+    title: 'Jones Hill',
+    description: `
+`,
+    image: require('./images/11.jpg'),
+    latitude: 51.3419,
+    longitude: -2.2531,
+    altitude: 45.68,
+    bearing: 15.79
   }
 ];
